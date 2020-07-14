07565237986923c17dda825d65c114d32e4ad658
wangyu <wangyu@sinoiov.com>
2020-07-14 20:40:51 +0800

    connectionpooldemo

--- thinkjava/src/com/conc/connpool/ConnectionDriver.java
+++ thinkjava/src/com/conc/connpool/ConnectionDriver.java
@@ -2,6 +2,7 @@ package com.conc.connpool;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.sql.Connection;
 import java.util.concurrent.TimeUnit;
 
@@ -15,5 +16,10 @@ public class ConnectionDriver {
             return null;
         }
     }
+        public static final Connection createConnection(){
+            return (Connection) Proxy.newProxyInstance(ConnectionDriver.class.getClassLoader(),
+                    new Class[]{Connection.class},new ConnectionHandler());
+        }
+
 
 }
--- thinkjava/src/com/conc/connpool/ConnectionPool.java
+++ thinkjava/src/com/conc/connpool/ConnectionPool.java
@@ -12,4 +12,34 @@ public class ConnectionPool {
             }
         }
     }
+    public void releaseConnection(Connection connection){
+        if(connection!=null){
+            synchronized (pool){
+                pool.addLast(connection);
+                pool.notifyAll();
+            }
+        }
+    }
+    public Connection fetchConnection(long mills) throws InterruptedException{
+        synchronized (pool){
+            if(mills<=0){//等待不超时，直到获取到连接
+                while(pool.isEmpty()){
+                    pool.wait();
+                }
+                return pool.removeFirst();
+            }else{
+                long future = System.currentTimeMillis()+mills;
+                long remaining = mills;
+                while(pool.isEmpty()&&remaining>0){
+                    pool.wait(remaining);
+                    remaining = future-System.currentTimeMillis();
+                }
+                Connection result = null;
+                if(!pool.isEmpty()){
+                    result=pool.removeFirst();
+                }
+                return result;
+            }
+        }
+    }
 }
